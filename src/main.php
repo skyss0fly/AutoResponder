256122b938efbdbf9acf94946963332512421300
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-05 11:41:22 +1000

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace skyss0fly\AutoResponder;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+class main extends PluginBase implements Listener {
+
+public function Trigger($event, $player) {
+  
+}
+}
